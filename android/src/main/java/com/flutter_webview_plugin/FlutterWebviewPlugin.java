eabfc78aba82bbe6f8e63bf59c2a34fb8860e4fc
zezo <zezohassam@gmail.com>
2022-01-15 13:11:12 +0200

    fixed bugs

--- android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
+++ android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
@@ -15,7 +15,11 @@ import android.os.Build;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import androidx.annotation.NonNull;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -24,10 +28,11 @@ import io.flutter.plugin.common.PluginRegistry;
 /**
  * FlutterWebviewPlugin
  */
-public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+public class FlutterWebviewPlugin implements FlutterPlugin, MethodCallHandler, PluginRegistry.ActivityResultListener {
     private Activity activity;
     private WebviewManager webViewManager;
     private Context context;
+    //private lateinit var channel : MethodChannel;
     static MethodChannel channel;
     private static final String CHANNEL_NAME = "flutter_webview_plugin";
     private static final String JS_CHANNEL_NAMES_FIELD = "javascriptChannelNames";
@@ -41,6 +46,9 @@ public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.A
         }
     }
 
+    public FlutterWebviewPlugin() {
+
+        }
     FlutterWebviewPlugin(Activity activity, Context context) {
         this.activity = activity;
         this.context = context;
@@ -324,4 +332,19 @@ public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.A
         }
         return false;
     }
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+            channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL_NAME);
+            context = binding.getApplicationContext();
+
+            channel.setMethodCallHandler(this);
+
+            //final FlutterWebviewPlugin instance = new FlutterWebviewPlugin(registrar.activity(), registrar.activeContext());
+            //registrar.addActivityResultListener(instance);
+
+            }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+            }
 }
